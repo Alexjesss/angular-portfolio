161e575f0718413f0b90b0cd6f98d5c05774cef1
Alexandra <banica@9221@hotmail.be>
2021-05-12 16:51:41 +0200

    trying to add parallax on scroll function to div image

--- portfolio/src/app/components/homepage/homepage.component.ts
+++ portfolio/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+
+  para = false;
+
+  @ViewChild('parallax') parallax: ElementRef;
+
+  @HostListener('document:scroll', ['$event'])
+  // tslint:disable-next-line:typedef
+  onWindowScroll(){
+    const windowScroll = window.pageYOffset;
+
+    if (windowScroll > this.parallax.nativeElement.offsetHeight){
+      this.para = true;
+    }
+    else {
+      this.para = false;
+    }
+  }
 
   constructor() { }
 
