fd8d4bda91eeb1a1bfab85bfaedc03862483280f
Alexandra <banica@9221@hotmail.be>
2021-05-12 11:48:32 +0200

    adding function to move window via mouse move

--- portfolio/src/app/components/landingPage/landingPage.component.ts
+++ portfolio/src/app/components/landingPage/landingPage.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,4 +20,11 @@ export class LandingPageComponent implements OnInit {
 function moveBackground(event): void {
 const mouseX = event.clientX;
 const mouseY = event.clientY;
+
+const calculateX = mouseX / (this.windowWidth / 7);
+const calculateY = mouseY / (this.windowHeight / 7);
+
+video.style.transform = 'translate(-' + calculateX.toString() + '%,-' + calculateY.toString() + '%)';
 }
+
+hero.addEventListener('mousemove', moveBackground(event));
